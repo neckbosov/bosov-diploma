b1d866dded522bdde35dcf1f10059739647ec739
Nikita Bosov <neckbosov@gmail.com>
2022-06-15 04:51:41 +0400

    final fixes

---
+++ slides/images/code_cpp.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(int argc, char **argv) {
+  string x = " _sMYL> kv";
+
+  if (size(x) < 15) {
+    printf("small");
+  } else {
+    printf("long");
+  }
+}
---
+++ slides/images/code_py.py
@@ -0,0 +1,10 @@
+def main():
+    x = "7Wg& [0m/ytuL"
+    if len(x) < 15:
+        print("small")
+    else:
+        print("long")
+
+
+if __name__ == '__main__':
+    main()
